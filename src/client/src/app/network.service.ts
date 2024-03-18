794dd3e0cc06260b6ed0bfe566e8c66c9432fc72
NikolaVilar <nikola.jordanovski@tridenstechnology.com>
2024-03-18 16:41:55 +0100

    git test on push

--- src/client/src/app/network.service.ts
+++ src/client/src/app/network.service.ts
@@ -16,7 +16,7 @@ export class NetworkService {
       fetch(url)
         .then((response) => {
           if (!response.ok) {
-            throw new Error('Network response was not ok');
+            throw new Error('Network response was not ok.');
           }
           return response.json();
         })
