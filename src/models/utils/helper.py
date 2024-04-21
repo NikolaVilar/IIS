9134495335fc5c694207710cad2f16781679a287
NikolaVilar <nikola.vilar@student.um.si>
2024-04-21 16:41:24 +0100

    fix: check

--- src/models/utils/helper.py
+++ src/models/utils/helper.py
@@ -12,7 +12,6 @@ import pandas as pd
 import mlflow
 from mlflow.models.signature import infer_signature
 from mlflow import MlflowClient
-import joblib
 
 def load_data(file_path):
     data = pd.read_csv(file_path, index_col=0)
